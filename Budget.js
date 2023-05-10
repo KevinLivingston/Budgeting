c265de1c0c311bd220afa8a2bbdb874c96e545b6
KevinLivingston <kevclivingston@gmail.com>
2023-05-09 23:20:05 -0400

    First Commit

---
+++ Budget.js
@@ -0,0 +1,10 @@
+var FixedAmount;
+var FixedAmounts= new Array();
+FixedAmount = document.getElementById("Expense_Initial");
+console.log(FixedAmount.innerText);
+var printVal = 23;
+FixedAmount.innerText = printVal;
+console.log(FixedAmount.innerText);
+
+
+
