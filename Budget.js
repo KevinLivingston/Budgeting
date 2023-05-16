b5abef403de0baea5a315be3d2f85909b97ba1e8
KevinLivingston <kevclivingston@gmail.com>
2023-05-15 22:28:29 -0400

    quick commit updated javascript file to access the initial table values

--- Budget.js
+++ Budget.js
@@ -1,10 +1,21 @@
-var FixedAmount;
-var FixedAmounts= new Array();
-FixedAmount = document.getElementById("Expense_Initial");
-console.log(FixedAmount.innerText);
-var printVal = 23;
-FixedAmount.innerText = printVal;
-console.log(FixedAmount.innerText);
+var expenses = [];
+var initialExpense= document.getElementById("Expense_Initial");
+expenses[0] = initialExpense.innerText;
+var expensesName = [];
+var expenseTypeInit =document.getElementById("Expense_Type");
+expensesName[0] = expenseTypeInit.innerText;
+var miscInit = document.getElementById("Misc_Remain");
+var miscMoney= [];
+miscMoney[0] = miscInit.innerText;
+var categoryMisc = document.getElementById("Misc_Type");
+var miscNames = [];
+miscNames[0]= miscName.innerText;
+//var fixed date, might be able to use the date object for instant 
+//real time update or allow a manual option for date selection
+
+
+
+
 
 
 
