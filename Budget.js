3acfd43c197ca3264061fa26c4ffbc7f0b378d30
KevinLivingston <kevclivingston@gmail.com>
2023-05-30 02:42:56 -0400

    Got some feedback from user input now just need to find a way to intergrate it into the table, Need one function to collect all day or one event to trigger all the necessary functions

--- Budget.js
+++ Budget.js
@@ -1,17 +1,40 @@
 var expenses = [];
-var initialExpense= document.getElementById("Expense_Initial");
+var initialExpense = document.getElementById("Expense_Initial");
 expenses[0] = initialExpense.innerText;
 var expensesName = [];
-var expenseTypeInit =document.getElementById("Expense_Type");
+var expenseTypeInit = document.getElementById("Expense_Type");
 expensesName[0] = expenseTypeInit.innerText;
 var miscInit = document.getElementById("Misc_Remain");
-var miscMoney= [];
+var miscMoney = [];
 miscMoney[0] = miscInit.innerText;
 var categoryMisc = document.getElementById("Misc_Type");
 var miscNames = [];
-miscNames[0]= miscName.innerText;
+miscNames[0] = categoryMisc.innerText;
+console.log("HI");
+console.log(document.getElementById("Misc").checked);
+
 //var fixed date, might be able to use the date object for instant 
 //real time update or allow a manual option for date selection
+function createEntry() {
+    var category = 2;
+
+}
+function findCategory() {
+    var catList = document.getElementsByName("Update");
+    for (var i = 0; i < catList.length; i++) {
+       if (catList[i].checked == true){
+        console.log(catList[i].id);
+       }
+    }
+}
+function storeName(){
+    console.log(document.getElementById('Category').value);
+}
+document.getElementById("submitBut").addEventListener('onclick',storeName, false);
+
+
+
+
 
 
 
